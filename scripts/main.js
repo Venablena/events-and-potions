9292dd9bdf16dcee41512cc4bd097ea6c5cc77ee
Lena Venable <venablena@gmail.com>
2017-09-14 09:12:42 -0700

    updated main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,47 @@
 // Tasks:
-
 // document.getElementById
 // Find just the title
 // Change the text of the title
 // Change the style of the title
 // Add a class to the title
 
-// document.querySelectorAll
-// Find all the list items
-// Find all list items of a specific list
-// Add a class of .green to all .plants list items
-// Add a class of .blue to all .liquids list items
-// Add a class of .red to all .animal-parts list items
 
-// document.createElement
-// Create a new ingredient from scratch!
-// Add it to one of the lists at the top
-// Add it to one of the lists at the bottom
-// Add it to one of the lists in the middle
+// document.addEventListener('DOMContentLoaded', function() {
+//   var title = document.getElementById('title');
+//   title.textContent = "I just changed!";
+//   title.style = "color: blue;"
+//   title.className = 'princess-font'
+//   var colors=['red', 'pink', 'blue', 'teal', 'aqua', 'orange', 'yellow', 'lime']
+//   var i=0;
+//   title.addEventListener('click', changeTitleColor)
+//   function changeTitleColor (){
+//       title.style.color = colors[i]
+//       i++;
+//     }
+//   }
+// });
+
+document.addEventListener('DOMContentLoaded', function() {
+var lis = document.querySelectorAll('li')
+  // console.log(lis)
+  for (var i = 0; i < lis.length; i++) {
+
+    var li = lis[i]
+    li.addEventListener('click', function(event){
+      console.log("I am clicking on a li", event.target);
+      event.target.style.color = "orange"
+      event.target.className += "selected"
+      let selected = document.querySelectorAll(".selected")
+      var myItems = ''
+      for (var j = 0; j < selected.length; j++) {
+        myItems += selected[j].textContent.trim() + " "
+      }
+      document.querySelector('.alert.alert-info span').textContent = myItems
+    })
+  }
+
+})
+
 
 // element.remove()
 // Find and remove specific items
